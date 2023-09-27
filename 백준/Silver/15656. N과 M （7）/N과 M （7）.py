6e9df0e7299d29327c39f6cda1c93b89e9b0a29b
KIM JIHYEON <84115348+JihyeonKim14@users.noreply.github.com>
2023-09-27 23:50:11 +0900

    [Silver III] Title: N과 M (7), Time: 2180 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,17 @@
+def recur(number):
+    if len(arr) == M:
+        print(*arr)
+        return
+    
+    for i,num in enumerate(num_list):
+        
+            arr.append(num)
+            recur(i+1)
+            arr.pop()
+
+N , M = map(int,input().split())
+num_list = list(map(int,input().split()))
+num_list.sort()
+
+arr = []
+recur(1)
